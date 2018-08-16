899d855e5f0241e1aad2b90aebc822322f9020af
Steve Giordano <stephenagiordano@gmail.com>
2018-08-15 21:15:30 -0400

    PT3

--- scripts/main.js
+++ scripts/main.js
@@ -63,7 +63,7 @@ get_data = ( function() {
             console.log(all_lists);
             if (!lists_list.length > 0) {
                 all_lists.forEach(list => {
-                    $("#user_lists").append('<option value="'+ list + '">' + list + "</option>");
+                    document.getElementById("user_lists").appendChild('<option value="'+ list + '">' + list + "</option>");
                 });
             }
 
